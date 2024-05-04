d8cbf48484d8b026675ae5ded2f8b6f38ae530b3
aydinSerbest <aydinserbest34@gmail.com>
2024-05-04 16:16:13 +0200

    updated

--- src/test/java/example7/Money.java
+++ src/test/java/example7/Money.java
@@ -1,14 +1,24 @@
 package example7;
 
 public class Money {
-    /*
-    A common superclass for two classes What if we had a Money class to capture the common equals code?
-     */
-    //The visibility has to change from private to protected so that the subclass can still see it.
 
     protected int amount;
     public boolean equals(Object object) {
         Money money = (Money) object;
-        return amount == money.amount;
+        return amount == money.amount
+        && getClass().equals(money.getClass());
     }
 }
+/*
+
+    The program does not compile.the old version :
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount:
+    }
+    revised.
+
+    the tests technically passed, meaning they did not produce an error,
+    but they did not produce the correct logical results.
+    Thus, the program may run without error, but it does not produce correct logical outcomes
+     */
