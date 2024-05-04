a50bd42efa498b45128557f2e111e4ab716d96fe
aydinSerbest <aydinserbest34@gmail.com>
2024-05-04 15:08:38 +0200

    updated

--- src/test/java/example7/MultiplyTest.java
+++ src/test/java/example7/MultiplyTest.java
@@ -2,8 +2,7 @@ package example7;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class MultiplyTest {
 
@@ -16,6 +15,21 @@ public class MultiplyTest {
 
         Franc fiveFrancs = new Franc(5);
         assertEquals(new Franc(10), fiveFrancs.times(2));
+    }
+    @Test
+    public void testEquality() {
+        assertTrue(new Dollar(10).equals(new Dollar(10)));
+        assertEquals(new Dollar(10), new Dollar(10));
+
+        assertFalse(new Dollar(15).equals(new Dollar(10)));
+        assertNotEquals(new Dollar(15), new Dollar(10));
+
+        assertTrue(new Franc(10).equals(new Franc(10)));
+        assertEquals(new Franc(10), new Franc(10));
+
+        assertFalse(new Franc(5).equals(new Franc(10)));
+        assertNotEquals(new Franc(5), new Franc(10));
+
     }
 }
 
