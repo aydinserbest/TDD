0554e144d6adaa77668471709f6385c4a4536669
aydinSerbest <aydinserbest34@gmail.com>
2024-05-08 23:41:51 +0200

    updated

--- src/test/java/example11/MultiplyTest.java
+++ src/test/java/example11/MultiplyTest.java
@@ -2,27 +2,23 @@ package example11;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class MultiplyTest {
-
+    /*
+    1. Quickly add a test.
+    2. Run all tests and see the new one fail.
+     */
+    /*
+    multi-currency money
+     */
+    // there is not Dollar class and methods and variables
+    //we only wrote test
     @Test
-    public void testCurrency() {
-        assertEquals("USD", Money.dollar(1).currency());
-        assertEquals("CHF", Money.franc(1).currency()); }
-    @Test
-    public void testFrancMultiplication() {
-        Money five = Money.franc(5);
-        assertEquals(Money.franc(10), five.times(2));
-        assertEquals(Money.franc(15), five.times(3)); }
-    @Test
-    public void testEquality() {
-        assertTrue(Money.dollar(5).equals(Money.dollar(5)));
-        assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-        assertTrue(Money.franc(5).equals(Money.franc(5)));
-        assertFalse(Money.franc(5).equals(Money.franc(6)));
-        assertFalse(Money.franc(5).equals(Money.dollar(5))); }
-
-
+    public void multiply(){
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+    }
 
 }
